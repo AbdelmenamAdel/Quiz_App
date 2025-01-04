fa7a1f572e35716ac35d8865485516a823e73215
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2025-01-04 18:18:29 +0200

    quistion card

---
+++ lib/core/widgets/question_card.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quiz/features/quiz/data/models/question_model.dart';
+import 'package:quiz/features/quiz/presentation/managers/quiz_cubit/quiz_cubit.dart';
+import 'answer_option.dart';
+
+class QuestionCard extends StatelessWidget {
+  final Question questionModel;
+
+  const QuestionCard({
+    super.key,
+    required this.questionModel,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+          height: 450,
+          margin: const EdgeInsets.symmetric(horizontal: 20.0),
+          decoration: BoxDecoration(
+            color: Colors.grey,
+            borderRadius: BorderRadius.circular(25.0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  questionModel.question,
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                //const SizedBox(height: 15),
+                const Spacer(
+                  flex: 1,
+                ),
+                ...List.generate(
+                    questionModel.answers.length,
+                    (index) => Column(
+                          children: [
+                            AnswerOption(
+                              answerModel: questionModel.answers[index],
+                              index: index,
+                              questionId: questionModel.questionId,
+                              onPressed: () {
+                                context
+                                    .read<QuizCubit>()
+                                    .checkAnswer(questionModel, index, context);
+                              },
+                            ),
+                            const SizedBox(
+                              height: 15,
+                            )
+                          ],
+                        )),
+                const Spacer(
+                  flex: 1,
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+}
