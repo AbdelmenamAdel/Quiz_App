16888f76577644bd547314a9a263b47b84c5e7d9
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2025-01-05 21:11:56 +0200

    remove the un used and reduce the states as possible

--- lib/features/quiz/presentation/managers/quiz_cubit/quiz_state.dart
+++ lib/features/quiz/presentation/managers/quiz_cubit/quiz_state.dart
@@ -5,25 +5,9 @@ sealed class QuizState {}
 
 class InitialState extends QuizState {}
 
-class StartedState extends QuizState {}
-
-class FinishedState extends QuizState {}
-
-class QuestionUpdatedState extends QuizState {
-  double progressValue;
-  QuestionUpdatedState(this.progressValue);
-}
-
-class TimerInitial extends QuizState {
-  final int secondsRemaining;
-  TimerInitial(this.secondsRemaining);
-}
-
 class TimerRunning extends QuizState {
   final int secondsRemaining;
   TimerRunning(this.secondsRemaining);
 }
-
-// class TimerCompleted extends QuizState {}
 
 class TimerStopped extends QuizState {}
